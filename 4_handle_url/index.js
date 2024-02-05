f9a1950f5f7841a54110e351488bbd3814298e21
Ankit <ak0330255@gmail.com>
2024-02-05 23:18:13 +0530

    handling_URL

---
+++ 4_handle_url/index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const myServer = http.createServer((req, res) => {
+  if (req.url == "/favicon.ico") return res.end();
+  const log = `${Date.now()}: ${req.url} New req recieved \n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+  fs.appendFile("log.txt", log, (err, data) => {});
+  switch (myUrl.pathname) {
+    case "/":
+      res.end("HomePage");
+      break;
+    case "/about":
+      const username = myUrl.query.myname;
+      res.end(`HI! ${username}`);
+      break;
+    default:
+      res.end("404 Not Found");
+      break;
+  }
+});
+
+myServer.listen(8000, () => {
+  console.log("Server Started at 8000");
+});
